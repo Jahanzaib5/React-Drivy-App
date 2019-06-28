11d00f3c0b11bb37b7683c8c9e8a5c3668bf6e66
Jahanzaib5 <50393893+Jahanzaib5@users.noreply.github.com>
2019-06-28 17:15:34 +0600

    further processing

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,5 +5,6 @@ import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 export const About = () => (
     <div>
       <h1>this is America</h1>
+      <p>so why don't you care</p>
     </div>
 )
