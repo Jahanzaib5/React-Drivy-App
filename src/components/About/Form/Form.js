e9819a5bfef432e180ad1438d2ffc295f0fb3f06
Jahanzaib5 <50393893+Jahanzaib5@users.noreply.github.com>
2019-06-30 15:07:19 +0600

    making in the process

--- src/components/About/Form/Form.js
+++ src/components/About/Form/Form.js
@@ -1,18 +1,10 @@
 import React, { Component } from "react";
-import {
-  Button,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  FormText,
-  Container,
-  Row,
-  Col
-} from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+
 import "./Form.css";
-import Pic from './01.JPG';
-import Image from 'react-bootstrap/Image';
+import Pic from "./01.JPG";
+import Image from "react-bootstrap/Image";
 
 export default class Forma extends Component {
   render() {
@@ -20,9 +12,74 @@ export default class Forma extends Component {
       <div>
         <Container>
           <Row>
-            <Col xs="6">.col-6</Col>
             <Col xs="6">
-                <Image src={Pic} height='500' alt='Apple'/>
+              <h1>This can take you to places</h1>
+              <Form>
+                <Row form>
+                  <Col md={6}>
+                    <FormGroup>
+                      <Input type="select" name="select" id="exampleSelect">
+                        <option>Volkswagen</option>
+                        <option>Toyota</option>
+                        <option>Mercedes</option>
+                        <option>Ferrari</option>
+                        <option>Buggati</option>
+                      </Input>
+                    </FormGroup>
+                  </Col>
+                  <Col md={6}>
+                    <FormGroup>
+                      <Input type="select" name="select" id="exampleSelect">
+                        <option>Cross Turan</option>
+                        <option>Bayern</option>
+                        <option>Dortmund</option>
+                        <option>Dusseldorf</option>
+                        <option>Leningard</option>
+                      </Input>
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <Row form>
+                  <Col md={6}>
+                    <FormGroup>
+                      <Input type="select" name="select" id="exampleSelect">
+                        <option>2019</option>
+                        <option>2016</option>
+                        <option>2014</option>
+                        <option>2015</option>
+                        <option>2013</option>
+                      </Input>
+                    </FormGroup>
+                  </Col>
+                  <Col md={6}>
+                    <FormGroup>
+                      <Input type="select" name="select" id="exampleSelect">
+                        <option>10-340 mi</option>
+                        <option>267-67 mi</option>
+                        <option>321-346- mi</option>
+                        <option>401-908 mi</option>
+                        <option>520 890 mi</option>
+                      </Input>
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <Row form>
+                  <Col md={6}>
+                    <FormGroup>
+                      <Input type="select" name="select" id="exampleSelect">
+                        <option>London</option>
+                        <option>Washigton DC</option>
+                        <option>Lords</option>
+                        <option>Oval</option>
+                        <option>Bermingham</option>
+                      </Input>
+                    </FormGroup>
+                  </Col>
+                </Row>
+              </Form>
+            </Col>
+            <Col xs="6">
+              <Image src={Pic} height="500" alt="Apple" />
             </Col>
           </Row>
         </Container>
