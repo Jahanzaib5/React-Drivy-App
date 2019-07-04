7beba8fa7b6034608e04d16eae408b0a5997cb1c
Jahanzaib5 <50393893+Jahanzaib5@users.noreply.github.com>
2019-07-04 11:05:32 +0600

    making in the process

--- src/components/About/LastCard/LastCard.js
+++ src/components/About/LastCard/LastCard.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
+import Card from 'react-bootstrap/Card';
 
 export default class LastCard extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col xs="6">
+            <Card>
+              <Card.Img variant="top" src="" />
+              <Card.Body>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs="6">
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
