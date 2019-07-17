a76fd9d1c94d605dee792436563de7261ab5cd14
Jahanzaib5 <50393893+Jahanzaib5@users.noreply.github.com>
2019-07-17 18:34:36 +0600

    processing

--- src/components/Drivers/GetDrivers.js
+++ src/components/Drivers/GetDrivers.js
@@ -2,11 +2,38 @@ import React, { Component } from "react";
 import { Container, Row, Col, Button } from "reactstrap";
 
 export default class GetDrivers extends Component {
+
+
+  state = {
+    users: []
+  };
+
+  componentDidMount = () => {
+    fetch("http://www.passenger.pk/api/driver?api_key=1234")
+      .then(res => res.json())
+      .then(info => {
+        console.log("Data: ", info);
+        const users = info.data.items; // []
+        this.setState({ users: users});
+      })
+      .catch(() => {});
+  };
+
+  displayUsers = () => {
+      return this.state.users.map((element, index, arr) => (
+          <img src={element.photo} height='100' alt=''/>
+      ));
+  };
+
   render() {
+
+    console.log('State: ', this.state);
     return (
       <div>
         <Container>
-          <h3>Drivers card will be here</h3>
+            <div>
+                {this.displayUsers()}
+            </div>
         </Container>
       </div>
     );
