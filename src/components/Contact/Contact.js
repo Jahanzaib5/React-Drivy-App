0610251603efc3507fd3efcc03a63df46499fb58
Jahanzaib5 <50393893+Jahanzaib5@users.noreply.github.com>
2019-07-17 11:28:38 +0600

    finalizing

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import FormU from './UserForm/FormU';
-import Output from './Login/Output';
 import Final from './Login/Final';
 
 export const Contact = () => (
--- src/components/Contact/Login/Final.js
+++ src/components/Contact/Login/Final.js
@@ -2,17 +2,26 @@ import React, { Component } from "react";
 import Form from "./InputForm";
 
 export default class Final extends Component {
-  getData = async e => {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      identity: null,
+      password: null,
+    };
+  }
+
+  getData = e => {
     e.preventDefault();
-    const identity = e.target.elements.identity.value;
-    const password = e.target.elements.password.value;
-    console.log(identity);
+    const identity = this.state.identity;
+    const password = this.state.password;
+    console.log(this.state.identity);
     console.log(password);
 
     var formData = new FormData();
-    formData.append("identity", identity);
+    formData.append("identity", e.target.elements.identity.value);
     formData.append("api_key", "1234");
-    formData.append("password", password);
+    formData.append("password", e.target.elements.password.value);
 
     fetch(`http://www.passenger.pk/api/user/login`, {
       method: "post",
@@ -39,7 +48,33 @@ export default class Final extends Component {
                 <div className="row">
                   <div className="col-xs-5 title-container" />
                   <div className="col-xs-7 form-container">
-                    <Form getData={this.getData} />
+                    <div className="wrapper">
+                      <div className="form-wrapper">
+                        <h1>Log in</h1>
+                        <form className="forme" onSubmit={this.getData} noValidate>
+                          <div className="firstName">
+                            <label htmlFor="identity">Identity</label>
+                            <input
+                              type="text"
+                              name="identity"
+                              placeholder="Identity..."
+                            />
+                          </div>
+                          <div className="lastName">
+                            <label htmlFor="password">Password</label>
+                            <input
+                              type="password"
+                              name="password"
+                              placeholder="Password..."
+                            />
+                          </div>
+
+                          <div className="createAccount">
+                            <button type="submit">Login</button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
